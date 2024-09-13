310b164549d8d2785094dc3511059e361a62de4e
Chris Zembower <chris.zembower@hashicorp.com>
2024-09-13 12:54:34 -0400

    publish models

---
+++ models/models.go
@@ -0,0 +1,69 @@
+package models
+
+import "encoding/json"
+
+type VaultInventory struct {
+	Namespaces []NamespaceInventory `json:"namespaces,omitempty"`
+	Usage      UsageData            `json:"usage,omitempty"`
+	Errors     []string             `json:"errors,omitempty"`
+}
+
+type NamespaceInventory struct {
+	Name           string          `json:"name,omitempty"`
+	AuthMounts     []AuthMount     `json:"authMounts,omitempty"`
+	SecretsEngines []SecretsEngine `json:"secretsEngines,omitempty"`
+	Entities       []Entity        `json:"entities,omitempty"`
+	Policies       []Policy        `json:"policies,omitempty"`
+	Errors         []string        `json:"errors,omitempty"`
+	Usage          UsageData       `json:"usage,omitempty"`
+}
+
+type AuthMount struct {
+	Path   string     `json:"path,omitempty"`
+	Type   string     `json:"type,omitempty"`
+	Roles  []AuthRole `json:"authRoles,omitempty"`
+	Users  []string   `json:"users,omitempty"`
+	Groups []string   `json:"groups,omitempty"`
+	Certs  []AuthRole `json:"certs,omitempty"`
+}
+
+type SecretsEngine struct {
+	Path      string   `json:"path,omitempty"`
+	Type      string   `json:"type,omitempty"`
+	Roles     []string `json:"roles,omitempty"`
+	Version   string   `json:"version,omitempty"`
+	Secrets   []string `json:"secrets,omitempty"`
+	ItemCount int      `json:"itemCount,omitempty"`
+}
+
+type Entity struct {
+	ID       string   `json:"id,omitempty"`
+	Name     string   `json:"name,omitempty"`
+	Policies []string `json:"policies,omitempty"`
+	Aliases  []Alias  `json:"aliases,omitempty"`
+}
+
+type Alias struct {
+	ID        string `json:"id,omitempty"`
+	Name      string `json:"name,omitempty"`
+	MountPath string `json:"mountPath,omitempty"`
+	MountType string `json:"mountType,omitempty"`
+}
+
+type AuthRole struct {
+	Name     string   `json:"name,omitempty"`
+	Policies []string `json:"policies,omitempty"`
+}
+
+type UsageData struct {
+	DistinctEntities json.Number `json:"distinctEntities,omitempty"`
+	Clients          json.Number `json:"clients,omitempty"`
+	NonEntityClients json.Number `json:"nonEntityClients,omitempty"`
+	SecretSyncs      json.Number `json:"secretSyncs,omitempty"`
+	AcmeClients      json.Number `json:"acmeClients,omitempty"`
+}
+
+type Policy struct {
+	Name  string   `json:"name,omitempty"`
+	Paths []string `json:"paths,omitempty"`
+}
